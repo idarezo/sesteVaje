bcfab3f778caf5ae78fcac148536da339d9d51bc
idarezo <99815226+idarezo@users.noreply.github.com>
2022-12-07 22:02:32 +0100

    Update vaje6.sh

--- vaje6.sh
+++ vaje6.sh
@@ -21,3 +21,4 @@ sudo apt-get update
 sudo apt-get -y install ca-certificates curl gnupg lsb-release
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
